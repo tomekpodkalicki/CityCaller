9a702b138d9210f434331889b809fdd1546036be
Podkal <tomek.podkalicki@o2.pl>
2024-09-20 20:56:54 +0200

    dependecies up 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
     namespace = "pl.podkal.citycaller"
     compileSdk = 34
+
 
     defaultConfig {
         applicationId = "pl.podkal.citycaller"
@@ -41,15 +43,50 @@ android {
 }
 
 dependencies {
+    //navi
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.navigation.dynamic.features.fragment)
+
+    //glide
+    implementation(libs.glide)
+
+    //coroutine
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.play.services)
+
+    //lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    //camera
+    implementation("androidx.camera:camera-core:1.2.3")
+    implementation("androidx.camera:camera-camera2:1.2.3")
+    implementation("androidx.camera:camera-lifecycle:1.2.3")
+    implementation("androidx.camera:camera-video:1.2.3")
+    implementation("androidx.camera:camera-view:1.2.3")
+    implementation(libs.androidx.camera.mlkit.vision.v100beta07)
+
+    //googlemaps
     implementation(libs.play.services.maps)
-    implementation(libs.firebase.bom)
+    implementation(libs.play.services.location)
+
+
+
+    //firebase
+    implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 
+    //ktx
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    //testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
